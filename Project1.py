489b0f565596547337623f4c2d33219e9ea693bf
jn1-name <62779053+jn1-name@users.noreply.github.com>
2023-10-29 21:31:36 -0400

    Add files via upload
    Adding Main File

---
+++ Project1.py
@@ -0,0 +1,21 @@
+#Example1:
+#   Enter the current temperature outside: 40
+#   Would you like to convert to Celsius? (Y/N): Y
+#   The temperature is 4.444444 degrees Celsius
+#
+#Example2:
+#   Enter the current temperature outside: 27
+#   Would you like to convert to Celsius? (Y/N): Y
+#   The temperature is -2.77777 degrees Celsius
+#
+#Example3:
+#   Enter the current temperature outside: 35
+#   Would you like to convert to Celsius? (Y/N): N
+#   The temperature is 35 degrees Fahrenheit
+
+
+
+# HINT, you will need this: ((current_temp) − 32) × 5/9 = 0°C
+
+
+
